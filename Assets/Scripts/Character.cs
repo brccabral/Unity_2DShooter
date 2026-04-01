2018634471fc66b6434e7e355d09a99b5d34bfda
Bruno Cabral <cabral.brc@gmail.com>
2026-04-01 15:55:00 -0700

    `abstract class` makes sure the script cannot be a component

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
     public Vector2 moveDirection;
     [SerializeField] private float moveSpeed;
