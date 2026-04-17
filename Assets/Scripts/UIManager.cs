4d6d4d8fad0a82513648a649b08360b6a3d279b9
Bruno Cabral <cabral.brc@gmail.com>
2026-04-16 17:55:20 -0700

    update health text color based on health percentage

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,7 +6,7 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI healthText;
-    
+
     private Player localPlayer;
     private GameManager localGameManager;
 
@@ -20,5 +20,7 @@ public class UIManager : MonoBehaviour
     {
         scoreText.text = localGameManager.GetCurrentScore().ToString();
         healthText.text = $"HEALTH: {localPlayer.health.GetHealthPoints():F1} %";
+        healthText.color = Color.Lerp(Color.red, Color.green,
+            Math.Clamp(localPlayer.health.GetHealthPoints(), 0, 100) / 100f);
     }
 }
